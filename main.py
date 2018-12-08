a2a7952e6d549db860698137a85d3b924bfb104b
Fatih Dartıcı <mfatihdartici@gmail.com>
2018-12-08 23:47:30 +0300

    pymongo added

--- main.py
+++ main.py
@@ -1,62 +1,60 @@
 from flask import Flask, request, render_template
 from flask_restful import reqparse, abort, Api, Resource
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
+#client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb://localhost:27017/")
+todoDB = client["todoDB"]
+todoCOL = todoDB["todo_list"]
+todoCOL.drop() # clear todo collection
+
+todolist   = [
+  { "name": "todo1", "task": "build an API"},
+  { "name": "todo2", "task": "?????"},
+  { "name": "todo3", "task": "profit!"}
+] # example data in list form
+
+todoCOL.insert_many(todolist) # inserting example data
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
 parser = reqparse.RequestParser()
 parser.add_argument('task')
-
 
 # Todo
 # shows a single todo item and lets you delete a todo item
 class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
 
     def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
+        #abort_if_todo_doesnt_exist(todo_id)
+        for i in todoCOL.find():
+            if i["name"]==todo_id:
+                todoCOL.delete_one(i)
         return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
 
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
     def get(self):
-        return TODOS
+        return [i for i in todoCOL.find({},{ "_id": 0, "name": 1, "task": 1 })] # update the todo list
 
     def post(self):
         args = parser.parse_args()
         try:
-            todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+            todo_id = len([i for i in todoCOL.find()]) + 1
             todo_id = 'todo%i' % todo_id
         except:
             todo_id = 'todo1'
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        todo_id = len([i for i in todoCOL.find()]) + 1
+        todo_id = 'todo%i' % todo_id
+        inserted_value = todoCOL.insert_one({'name':todo_id,'task': args['task']})
+        return {'name':todo_id,'task': args['task']}, 201
 
 ##
 ## Actually setup the Api resource routing here
